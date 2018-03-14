8b9e9d5ee7d0e4a59b19662f1caef3c3ac89e7ec
Samuel Maxey <smaxey@xula.edu>
2018-03-14 13:19:52 -0500

    BACK TOL BUSINESS

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("\nHello, Assignments App!\n");
+        assignment = new assignment();
+        assignment1 = new assignment1();
 
 
 
