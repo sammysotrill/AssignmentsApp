85982173566a8e87cffa12a79eef7140682f68f6
Samuel Maxey <smaxey@xula.edu>
2018-03-13 22:38:51 -0500

    BACK TOL BUSINESS

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,27 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
+    public enum day{
+        MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+
+    }
+
+    public enum courses{
+        CALC2,DATASTRUCTURES,WORLDCIVILIZATIONS,SPANISH
+    }
+
+    public enum category{
+        HOMEWORK,QUIZ,TEST,PRESENTATION,FINAL_EXAM
+    }
+
     public static void main(String[] args) {
-
-        System.out.println("\n\nHello, AssignmentsApp!\n");
-        // Output the current date-time
-        LocalDateTime today = LocalDateTime.now();
-        System.out.println("Today's date and time is: " + today);
-
-        //Output tomorrow's date using a formatter
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
-        String formatDateTime = today.format(formatter);
-        System.out.println("\n" + "The newly formatted date and time: " + formatDateTime);
-
-        //Add five weeks to today's LocalDateTime
-        LocalDateTime fiveWeeksLater = addFiveWeeks(today);
-        System.out.println("\n" + "Five weeks later....." + fiveWeeksLater);
-
-        //Initialize a LocalDateTime object to your birthdate and the time 12:35pm
-        LocalDateTime bornDayWithTime = birthDateTime(2,19,1998);
-        System.out.println("\n" + "My born day along with a given time: " + bornDayWithTime);
-
-        //Output the day of the week (Sunday-Saturday) that you were born
-        System.out.println( "\n" + "The exact day that I was born: " + bornDayWithTime.getDayOfWeek() );
-
+        System.out.println("\nHello, Assignments App!\n");
 
 
 
     }
-
-    private static LocalDateTime birthDateTime(int month, int day, int year) {
-        LocalDateTime answer = LocalDateTime.of(year,month,day,12,35);
-
-        return answer;
-
-    }
-
-    private static LocalDateTime addFiveWeeks(LocalDateTime date) {
-        LocalDateTime answer  = date.plusWeeks(5);
-
-        return answer;
-
-    }
-
 }
 
