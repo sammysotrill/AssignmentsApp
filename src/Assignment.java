39e5d7025f038e38d4c9240618816f547b9fffa2
Samuel Maxey <smaxey@xula.edu>
2018-03-18 21:40:20 -0500

    restart

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,2 +1,24 @@
+import java.time.LocalDateTime;
+
 public class Assignment {
+    private LocalDateTime time;
+    private int priority;
+    //private enum category;
+    //private enum course;
+
+
+    public Assignment(){
+        time = LocalDateTime.now();
+        priority = 3;
+    }
+
+    @Override
+    public String toString() {
+        return time + " | " + "PRIORITY: " + priority;
+    }
+
+    public Assignment(LocalDateTime time, int priority){
+        this.time = time;
+        this.priority = priority;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.time.LocalDateTime;
 
 public class Main {
 
@@ -24,7 +24,16 @@ public class Main {
 
         // generate 2 random assignments
         Assignment assign1 = new Assignment();
+        System.out.println("What is the first assignment? " + assign1 + "\n");
+        Assignment assign2 = new Assignment(LocalDateTime.now(),2);
+        System.out.println("What is the second assignment?" + assign2 + "\n");
+
+        // copy assignment 1 to assignment three
+        Assignment assign3 = new Assignment();
+        System.out.println("What is the third assignment? " + assign3 + "\n");
+
         }
+
 
 
     }
